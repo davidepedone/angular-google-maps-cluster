efd6f0385024c3010efbc8221c6d8b78995021ea
Davide Pedone <davide.pedone@gmail.com>
2015-12-16 11:54:33 +0100

    v0.1.0

--- src/js/ngmapcluster.js
+++ src/js/ngmapcluster.js
@@ -27,7 +27,6 @@
                     zoom: 1,
                 };
                 var markerIconUrl = 'http://marker.local/sample/building.png';
-
                 mapOptions.mapTypeId = google.maps.MapTypeId.ROADMAP;
                 // # Get place from coords and Set map center
                 mapOptions.center = new google.maps.LatLng(0.1700235000, 20.7319823000);
@@ -48,17 +47,8 @@
                 if ($scope.responsive && $scope.responsive == 'true') {
                     canvas.className += ' responsive';
                 }
-                /*
-                google.maps.event.addListener(map, 'idle', showMarkers);
-                function showMarkers() {
-                    var bounds = map.getBounds();
-                    console.log(bounds);
-                    // Call you server with ajax passing it the bounds
-
-                    // In the ajax callback delete the current markers and add new markers
-                }*/
-                var markerClick = function(marker){
-                    return function(){
+                var markerClick = function(marker) {
+                    return function() {
                         $scope.onClickCallback(marker, map);
                     };
                 };
@@ -67,92 +57,16 @@
                     marker = new google.maps.Marker({
                         position: pos,
                         map: map,
-                        originalObject : $scope.places[i],
-                        icon : markerIconUrl
-                            /*,
-                                                    title : 'Title',
-                                                    label : 'Label',*/
+                        originalObject: $scope.places[i],
+                        icon: markerIconUrl
                     });
-                    //var storyClick = new Function("event", "alert('Click on marker " + i + " ');");
-                    /*
-                    google.maps.event.addListener(marker, 'click', function(){
-                        alert('Click on marker '+i);
-                    });
-                    */
                     google.maps.event.addListener(marker, 'click', markerClick(marker));
                     marker_list.push(marker);
                 }
-                /*
-                for (var i = 0; i < 1000; i++) {
-                    //var a = markers[i].coordinate;
-                    //pos = new google.maps.LatLng(a[1], a[0]);
-                    pos = new google.maps.LatLng(Math.floor(Math.random() * 50), Math.floor(Math.random() * 100));
-                    marker = new google.maps.Marker({
-                        position: pos,
-                        map: map,
-                        title: 'Title'//,
-                        //label: 'Label',
-                        //animation: google.maps.Animation.DROP
-                    });
-                    var storyClick = new Function("event", "alert('Click on marker " + i + " ');");
-                    google.maps.event.addListener(marker, 'click', storyClick);
-                    marker_list.push(marker);
-                }
-                */
-                /*
-                                var stylez = [{
-                                    height: 53,
-                                    width: 53,
-                                    url: "http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/images/m1.png"
-                                },{
-                                    height: 53,
-                                    width: 53,
-                                    url: "http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/images/m1.png"
-                                },{
-                                    height: 53,
-                                    width: 53,
-                                    url: "http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/images/m1.png"
-                                },{
-                                    height: 53,
-                                    width: 53,
-                                    url: "http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/images/m1.png"
-                                },{
-                                    height: 53,
-                                    width: 53,
-                                    url: "http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/images/m1.png"
-                                },{
-                                    height: 53,
-                                    width: 53,
-                                    url: "http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/images/m1.png"
-                                },
-                                {
-                                    height:50,
-                                    width:50,
-                                    url:"https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=50&h=50"
-                                }];
-                */
                 var markerCluster = new MarkerClusterer(map, marker_list, {
                     gridSize: 40,
-                    minimumClusterSize: 2,
-                    /*
-                    calculator: function(markers, numStyles) {
-                        console.log('@@@@@@@@@@@@@@@@@@@@@@@@');
-                        console.log(arguments);
-                        console.log('@@@@@@@@@@@@@@@@@@@@@@@@');
-                        return {
-                            text: markers.length,
-                            index: numStyles
-                        };
-                    },
-                   styles: stylez
-                    */
+                    minimumClusterSize: 2
                 });
-                //markerCluster.setStyles({});
-                /*
-                console.log('@@@@@@@@@@@@@@@@@@@');
-                console.log(markerCluster.getStyles());
-                console.log('@@@@@@@@@@@@@@@@@@@');
-                */
                 markerCluster.setCalculator(function(markers, numStyles) {
                     return {
                         text: markers.length,
