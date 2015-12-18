5a4be3059c82427d506084a8beb8d147012d1f9f
Davide Pedone <davide.pedone@gmail.com>
2015-12-18 16:55:59 +0100

    custom clusterCalc & markerIcon

--- src/js/ngmapcluster.js
+++ src/js/ngmapcluster.js
@@ -17,7 +17,11 @@
                 readonly: '@?',
                 responsive: '@?',
                 draggable: '@?',
-                onClickCallback: '=?'
+                onClickCallback: '=?',
+                markerIcon: '=?',
+                clusterIcon: '=?',
+                clusterCalculator: '=?',
+                updateMarker: '=?'
             },
             controller: ['$scope', function($scope) {}],
             template: '<div class="dp-places-map-wrapper"><div class="dp-places-map-canvas"></div></div>',
@@ -26,7 +30,6 @@
                 var mapOptions = {
                     zoom: 1,
                 };
-                var markerIconUrl = 'https://raw.githubusercontent.com/davidepedone/angular-google-maps-cluster/master/sample/building.png';
                 mapOptions.mapTypeId = google.maps.MapTypeId.ROADMAP;
                 // # Get place from coords and Set map center
                 mapOptions.center = new google.maps.LatLng(0.1700235000, 20.7319823000);
@@ -35,7 +38,8 @@
                 var map = new google.maps.Map(canvas, mapOptions);
                 var pos;
                 var marker;
-                var marker_list = [];
+                var markerList = [];
+                var markerDictionary = {};
                 if ($scope.draggable && $scope.draggable == 'false') {
                     mapOptions.draggable = false;
                     mapOptions.scrollwheel = false;
@@ -52,27 +56,81 @@
                         $scope.onClickCallback(marker, map);
                     };
                 };
-                for (var i = 0; i < $scope.places.length; i++) {
-                    pos = new google.maps.LatLng($scope.places[i].latitude, $scope.places[i].longitude);
+                var getMarkerIcon = function(object) {
+                    if ($scope.markerIcon && typeof $scope.markerIcon === 'function') {
+                        return $scope.markerIcon(object);
+                    } else {
+                        // will use default google maps marker icon
+                        return null;
+                    }
+                };
+                var markerIconUrl = '';
+                var createMarker = function(object) {
+                    pos = new google.maps.LatLng(object.latitude, object.longitude);
+                    markerIconUrl = getMarkerIcon(object);
                     marker = new google.maps.Marker({
                         position: pos,
                         map: map,
-                        originalObject: $scope.places[i],
+                        originalObject: object,
                         icon: markerIconUrl
                     });
                     google.maps.event.addListener(marker, 'click', markerClick(marker));
-                    marker_list.push(marker);
+                    // If marker is already in dictionary delete it
+                    if (markerDictionary[object.id]) {
+                        delete markerDictionary[object.id];
+                    }
+                    // Add to dictionary
+                    markerDictionary[object.id] = marker;
+                    return marker;
+                };
+                for (var i = 0; i < $scope.places.length; i++) {
+                    markerList.push(createMarker($scope.places[i]));
                 }
-                var markerCluster = new MarkerClusterer(map, marker_list, {
+                $scope.updateMarkerInternal = $scope.updateMarker || {};
+                $scope.updateMarkerInternal.bulkUpdate = function(markersToUpdate) {
+                    if (Object.prototype.toString.call(markersToUpdate) !== '[object Array]') {
+                        return;
+                    }
+                    var markersToRemove = [];
+                    var markersToAdd = [];
+                    var currentProcesssingMarker = {};
+                    var currentId = '';
+                    var repaint = false;
+                    for (var i = 0, markers = markersToUpdate.length; i < markers; i++) {
+                        currentProcesssingMarker = markersToUpdate[i];
+                        currentId = currentProcesssingMarker.id;
+                        try {
+                            // get previous marker instance
+                            markersToRemove.push(markerDictionary[currentId]);
+                            // add new instance
+                            markersToAdd.push(createMarker(currentProcesssingMarker));
+                        } catch (e) {}
+                    }
+                    if (markersToRemove.length > 0) {
+                        repaint = true;
+                        markerCluster.removeMarkers(markersToRemove);
+                    }
+                    if (markersToAdd.length > 0) {
+                        repaint = true;
+                        markerCluster.addMarkers(markersToAdd);
+                    }
+                    if (repaint) {
+                        markerCluster.repaint();
+                    }
+                };
+                var markerClusterOptions = {
                     gridSize: 40,
                     minimumClusterSize: 2
-                });
-                markerCluster.setCalculator(function(markers, numStyles) {
-                    return {
-                        text: markers.length,
-                        index: numStyles
-                    };
-                });
+                };
+                if ($scope.clusterIcon && typeof $scope.clusterIcon === 'object') {
+                    markerClusterOptions.styles = $scope.clusterIcon;
+                }
+                var markerCluster = new MarkerClusterer(map, markerList, markerClusterOptions);
+                if ($scope.clusterCalculator && typeof $scope.clusterCalculator === 'function') {
+                    markerCluster.setCalculator(function(marker, totalStyles) {
+                        return $scope.clusterCalculator(marker, totalStyles);
+                    });
+                }
             }
         };
     });
